c8fc5c92d4761b9516d9cfa676445a00498369dc
Nils Olsson <nilso@enosis.net>
2018-01-22 12:30:41 -0800

    Update Tarkov (refactor JS, change factory content)

--- pages/vidya/tarkov/js/main.js
+++ pages/vidya/tarkov/js/main.js
@@ -1,18 +1,14 @@
 $(function() {
-  $('.checklist-header').each(function() {
-    addCollapse(this);
-    $(this).find('a.btn-collapse').click(function() {
-      $(this).find('svg').toggleClass('fa-folder-open fa-folder');
-    });
+  var items = $('.checklist li');
+  items.each(function(i, ele) {
+    $(ele).attr('data-id', i);
   });
-  $('.checklist').each(function() {
-    var checklistDataId = $(this).attr('data-id');
-    var itemDataIdPrefix = checklistDataId.substr(0, checklistDataId.length - 1);
-    $(this).find('li').each(function(i, ele) {
-      $(ele).attr('data-id', itemDataIdPrefix + '_' + ('00' + (i+1)).slice(-2));
-      addCheckbox($(ele));
-    });
-  });
+
+  // Construct checklist headers
+  $('.checklist-header').each(constructHeader);
+
+  // Construct checklists and checkboxes
+  $('.checklist').each(constructChecklist);
 });
 
 
@@ -27,38 +23,48 @@ String.prototype.format = function () {
 
 
 collapserBtnTemplate =
-'<a href="#{0}_col" data-toggle="collapse" class="btn btn-primary btn-collapse btn-sm collapsed">' +
+'<a href="#{0}" data-toggle="collapse" class="btn btn-primary btn-collapse btn-sm collapsed">' +
 '<i class="fas fa-folder"></i>' +
 '</a>' +
 '<span class="align-middle">{1}</span>';
 
+
 checkTemplate =
-'<div class="checkbox">' +
 '<label>' +
-'<input type="checkbox" id="{0}">' +
-'<span class="item_content">{1}</span>' +
-'</label>' +
-'</div>';
+'<input type="checkbox">' +
+'{0}' +
+'</label>';
 
 
-function addCollapse(ele) {
-  var $ele = $(ele);
-  var content = $ele.html();
-  content = collapserBtnTemplate.format($ele.attr('id'), content);
-  $ele.html(content);
+function constructHeader(_, ele) {
+  var content = $(ele).html();
+  content = collapserBtnTemplate.format($(ele).attr('data-target'), content);
+  $(ele).html(content);
+  $(ele).find('a.btn-collapse').click(function() {
+    $(ele).find('svg').toggleClass('fa-folder-open fa-folder');
+  });
 }
 
 
-function addCheckbox(ele) {
-  var $ele = $(ele);
-  var content = $ele.html();
-  var sublists = $ele.children('ul');
-  content = checkTemplate.format($ele.attr('data-id'), content);
-  $ele.html(content).append(sublists);
-  var check = $ele.find('input');
-  check.prop('checked', $.jStorage.get(check.attr('id'), false));
+function constructChecklist(_, ele) {
+  var checkboxIdPrefix = $(ele).attr('id');
+  $(ele).find('li').each(function(i, item) {
+    $(item).attr('id', checkboxIdPrefix + '-' + (i+1));
+    constructCheckbox($(item));
+  });
+}
+
+
+function constructCheckbox(ele) {
+  var content = $(ele).html();
+  var sublists = $(ele).children('ul');
+  content = checkTemplate.format(content);
+  $(ele).html(content).append(sublists);
+
+  var dataId = $(ele).attr('data-id');
+  var check = $(ele).find('input');
+  check.prop('checked', $.jStorage.get(dataId, false));
   check.click(function() {
-    var checkId = $(this).attr('id');
-    $.jStorage.set(checkId, $(this).prop('checked'));
+    $.jStorage.set(dataId, $(check).prop('checked'));
   });
 }
--- pages/vidya/tarkov/key_lists/factory.md
+++ pages/vidya/tarkov/key_lists/factory.md
@@ -1,7 +1,18 @@
-- [Factory Key][01] This should never leave your gamma and opens two additional extracts and allows you to open the locked room in third floor offices. Possible weapon spawn in locked room, two coats, loose loot and occasional high end ammo. This key spawns in the office of 3-Storey in Customs, and the glass warehouse in Customs (in the blue locker or near the dead scav).
-- [Key (pumping station)][02] Opens the "closed bunker" near machinery in the exit area but only the door nearest extract. There is rarely anything worth picking up in here but it can be a good ambush position. This key spawns in the locker room on the bench against the wall close to the entrance. 
-- [Key (admin)][03] Opens the "closed bunker" near machinery in the exit area. There is rarely anything worth picking up in here but it can be a good ambush position. This is a random drop now. 
+- [Factory Key][01] This should never leave your gamma. On factory it opens two
+  additional extracts (which?) and allows you to open the locked room in third
+  floor offices which has a possible weapon spawn, two coats, loose loot and
+  occasional high end ammo. Where this key can be found: Customs 3-Story dorm
+  guard station; Customs glass warehouse in the blue locker; Customs glass
+  warehouse on the couch next to the dead scav; Customs checkpoint car trunk,
+  possibly in jacket therein; in scav backpacks.
+- [Key (pumping station)][02] Opens the "closed bunker" near machinery in the
+  exit area but only the door nearest extract. There is rarely anything worth
+  picking up in here but it can be a good ambush position. This key spawns in
+  the locker room on the bench against the wall close to the entrance. 
+- [Key (admin)][03] Opens the "closed bunker" near machinery in the exit area.
+  There is rarely anything worth picking up in here but it can be a good ambush
+  position. This is a random drop now. 
 
-[01]: #
+[01]: https://escapefromtarkov.gamepedia.com/Factory_exit_key
 [02]: #
 [03]: #
