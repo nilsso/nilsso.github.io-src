11d999f25ced047827d369735869f4b8c4a507bb
Nils Olsson <nilso@enosis.net>
2020-03-18 20:46:34 -0700

    Lazy update

--- push.sh
+++ push.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 function acp {
-  git add -A;
-  git commit -m '$1';
-  git push;
+    git add -A;
+    git commit -m "$@"
+    git push;
 }
-bundle exec jekyll b -b 'https://nilsso.github.io'
-(cd _site; acp 'Lazy update (live)')
-acp 'Lazy update (source)'
+
+msg="$@"
+acp "$msg"
+#bundle exec jekyll b -I -b 'https://nilsso.github.io'
+#(cd _site; acp "$@ (live)")
+#acp $msg
